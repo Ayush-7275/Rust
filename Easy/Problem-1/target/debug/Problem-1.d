288a8ade3cc933e5144e6b408edbfc711173d1fe
Ayushman Rai <raiayush72757@gmail.com>
2026-05-20 23:13:32 +0530

    Remove build artifacts

--- Easy/Problem-1/target/debug/Problem-1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Ayush\Projects\Rust\Easy\Problem-1\target\debug\Problem-1.exe: C:\Users\Ayush\Projects\Rust\Easy\Problem-1\src\main.rs
--- Easy/Problem-1/target/debug/deps/Problem_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ayush\Projects\Rust\Easy\Problem-1\target\debug\deps\Problem_1.d: src\main.rs
-
-C:\Users\Ayush\Projects\Rust\Easy\Problem-1\target\debug\deps\Problem_1.exe: src\main.rs
-
-src\main.rs:
